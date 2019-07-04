b1236acf6ad4f3cbe8a1a6267a25b87ab7a452bc
Sanja <pavlova.sanja@yahoo.com>
2019-07-04 23:11:46 +0200

    example project for issue reproduction

---
+++ nightwatch.conf.js
@@ -0,0 +1,40 @@
+const { config } = require('dotenv-safe');
+
+config();
+
+module.exports = {
+  test_settings: {
+    default: {
+      selenium: {
+        start_process: false,
+        host: 'hub-cloud.browserstack.com',
+        port: 80
+      },
+      desiredCapabilities: {
+        'browserstack.user': 'sanja24',
+        'browserstack.key': 'BxTS45SxnNsfap28iFyL',
+		"browserstack.selenium_version": "3.14.0",
+        os: 'Windows',
+        os_version: '10'
+      }
+    },
+    chrome: {
+      desiredCapabilities: {
+        browserName: 'chrome',
+        javascriptEnabled: true,
+        acceptSslCerts: true,
+        chromeOptions: {
+          args: ['disable-gpu']
+        }
+      }
+    },
+    firefox: {
+      desiredCapabilities: {
+        browserName: 'firefox',
+        javascriptEnabled: true,
+        acceptSslCerts: true,
+        marionette: true
+      }
+    }
+  }
+};
